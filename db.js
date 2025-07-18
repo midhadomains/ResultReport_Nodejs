baf6c1c65eb33711314920950a6cfed778bcf79a
Midha <159537481+midhadomains@users.noreply.github.com>
2025-07-18 17:21:00 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,34 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const connection = mongoose.connect(process.env.mongoURL);
+
+
+
+// module.exports = { connection }
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connection = mongoose.connect(process.env.mongoURL);
+let isConnected = false;
 
+const connectDB = async () => {
+  if (isConnected) {
+    return;
+  }
 
+  try {
+    const conn = await mongoose.connect(process.env.mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-module.exports = { connection }
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    throw err;
+  }
+};
+
+module.exports = { connectDB };
